7b6e7cc4adf8e14841b00c188406e20132b6b7ea
Rustam <haganeninja@gmail.com>
2020-10-23 12:33:20 +0700

    some work)

---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hexlet\Php\Runner;
+
+use Tightenco\Support\Collection;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null]) ->map(function($name) {
+        return strtoupper($name);
+    });
+}
